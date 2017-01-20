0a1409069ffeacae81b081776c1d03a7a06eb5e3
Flow <floryn.euro.2007@gmail.com>
2017-01-20 10:58:41 +0200

    added the ability to edit subjects

--- public/manage_content.php
+++ public/manage_content.php
@@ -5,25 +5,26 @@
 <?php find_selected_page(); ?>
 
 <div id="main">
-  <div id="navigation">
-		<?php echo navigation($current_subject, $current_page); ?>
-		<br />
-		<a href="new_subject.php">+ Add a subject</a>
-  </div>
-  <div id="page">
-		<?php echo message(); ?>
-		<?php if ($current_subject) { ?>
-	    <h2>Manage Subject</h2>
-			Menu name: <?php echo $current_subject["menu_name"]; ?><br />
-			
-		<?php } elseif ($current_page) { ?>
-			<h2>Manage Page</h2>
-			Menu name: <?php echo $current_page["menu_name"]; ?><br />
-			
-		<?php } else { ?>
-			Please select a subject or a page.
-		<?php }?>
-  </div>
+    <div id="navigation">
+        <?php echo navigation($current_subject, $current_page); ?>
+        <br />
+        <a href="new_subject.php">+ Add a subject</a>
+    </div>
+    <div id="page">
+        <?php echo message(); ?>
+        <?php if ($current_subject) { ?>
+            <h2>Manage Subject</h2>
+            Menu name: <?php echo $current_subject["menu_name"]; ?><br />
+            <a href="edit_subject.php?subject=<?php echo $current_subject["id"]; ?>">Edit subject</a>
+
+        <?php } elseif ($current_page) { ?>
+            <h2>Manage Page</h2>
+            Menu name: <?php echo $current_page["menu_name"]; ?><br />
+
+        <?php } else { ?>
+            Please select a subject or a page.
+        <?php } ?>
+    </div>
 </div>
 
 <?php include("../includes/layouts/footer.php"); ?>
