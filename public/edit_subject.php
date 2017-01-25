43adc0614407bfb86084f7e46e05128696e6ad9e
Flow <floryn.euro.2007@gmail.com>
2017-01-25 10:31:39 +0200

    added the ability to edit subjects

--- public/edit_subject.php
+++ public/edit_subject.php
@@ -1,7 +1,7 @@
 <?php require_once("../includes/session.php"); ?>
 <?php require_once("../includes/db_connection.php"); ?>
 <?php require_once("../includes/functions.php"); ?>
-
+<?php require_once("../includes/validation_functions.php"); ?>
 <?php find_selected_page(); ?>
 
 <?php
@@ -13,17 +13,66 @@ if (!$current_subject) {
 ?>
 <?php include("../includes/layouts/header.php"); ?>
 
+
+
+<?php
+if (isset($_POST['submit'])) {
+    // Process the form
+    // validations
+    $required_fields = array("menu_name", "position", "visible");
+    validate_presences($required_fields);
+
+    $fields_with_max_lengths = array("menu_name" => 30);
+    validate_max_lengths($fields_with_max_lengths);
+
+    if (empty($errors)) {
+        // Perform update
+        $id = $current_subject["id"];
+        $menu_name = mysql_prep($_POST["menu_name"]);
+        $position = (int) $_POST["position"];
+        $visible = (int) $_POST["visible"];
+
+        $query = "UPDATE subjects SET ";
+        $query .= "menu_name = '{$menu_name}', ";
+        $query .= "position = {$position}, ";
+        $query .= "visible = {$visible} ";
+        $query .= "WHERE id = {$id} ";
+        $query .= "LIMIT 1";
+
+        $result = mysqli_query($connection, $query);
+
+        if ($result && mysqli_affected_rows($connection) == 1) {
+            // Success
+            $_SESSION["message"] = "Subject updated.";
+            redirect_to("manage_content.php");
+        } else {
+            // Failure
+            $message = "Subject update failed.";
+        }
+    }
+} else {
+    // This is probably a GET request
+}
+?>
+
+
+
+
+
 <div id="main">
     <div id="navigation">
         <?php echo navigation($current_subject, $current_page); ?>
     </div>
     <div id="page">
-        <?php echo message(); ?>
-        <?php $errors = errors(); ?>
+        <?php
+        if (!empty($message)) {
+            echo "<div class=\"message\">" . $message . "</div>";
+        }
+        ?>
         <?php echo form_errors($errors); ?>
 
         <h2>Edit Subject: <?php echo $current_subject["menu_name"]; ?></h2>
-        <form action="create_subject.php" method="post">
+        <form action="edit_subject.php?subject=<?php echo $current_subject["id"]; ?>" method="post">
             <p>Menu name:
                 <input type="text" name="menu_name" value="<?php echo $current_subject["menu_name"]; ?>" />
             </p>
@@ -44,16 +93,18 @@ if (!$current_subject) {
             </p>
             <p>Visible:
                 <input type="radio" name="visible" value="0"  
-                    <?php if ($current_subject["visible"] == 0) {
-                        echo "checked";
-                    }
-                    ?>/> No
+                <?php
+                if ($current_subject["visible"] == 0) {
+                    echo "checked";
+                }
+                ?>/> No
                 &nbsp;
                 <input type="radio" name="visible" value="1" 
-                    <?php if ($current_subject["visible"] == 1) {
-                        echo "checked";
-                    }
-                    ?> /> Yes
+                <?php
+                if ($current_subject["visible"] == 1) {
+                    echo "checked";
+                }
+                ?> /> Yes
             </p>
             <input type="submit" name="submit" value="Edit Subject" />
         </form>
