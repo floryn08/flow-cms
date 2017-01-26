8c8fdfad4fd429fcbccbfce68332fbbc52fe19e8
Flow <floryn.euro.2007@gmail.com>
2017-01-26 10:40:26 +0200

    cleaned up the code, added some css for error display, return to main menu button, auto copyright

--- public/edit_subject.php
+++ public/edit_subject.php
@@ -41,7 +41,7 @@ if (isset($_POST['submit'])) {
 
         $result = mysqli_query($connection, $query);
 
-        if ($result && mysqli_affected_rows($connection) == 1) {
+        if ($result && mysqli_affected_rows($connection) >= 0) {
             // Success
             $_SESSION["message"] = "Subject updated.";
             redirect_to("manage_content.php");
@@ -66,15 +66,15 @@ if (isset($_POST['submit'])) {
     <div id="page">
         <?php
         if (!empty($message)) {
-            echo "<div class=\"message\">" . $message . "</div>";
+            echo "<div class=\"message\">" . htmlentities($message) . "</div>";
         }
         ?>
         <?php echo form_errors($errors); ?>
 
-        <h2>Edit Subject: <?php echo $current_subject["menu_name"]; ?></h2>
-        <form action="edit_subject.php?subject=<?php echo $current_subject["id"]; ?>" method="post">
+        <h2>Edit Subject: <?php echo htmlentities($current_subject["menu_name"]); ?></h2>
+        <form action="edit_subject.php?subject=<?php echo urlencode($current_subject["id"]); ?>" method="post">
             <p>Menu name:
-                <input type="text" name="menu_name" value="<?php echo $current_subject["menu_name"]; ?>" />
+                <input type="text" name="menu_name" value="<?php echo htmlentities($current_subject["menu_name"]); ?>" />
             </p>
             <p>Position:
                 <select name="position">
@@ -110,7 +110,7 @@ if (isset($_POST['submit'])) {
         </form>
         <br />
         <a href="manage_content.php">Cancel</a> <br><br>
-        <a href="delete_subject.php?subject=<?php echo $current_subject["id"]; ?>" onclick="return confirm('Are you sure?');">Delete subject</a>
+        <a href="delete_subject.php?subject=<?php echo urlencode($current_subject["id"]); ?>" onclick="return confirm('Are you sure?');">Delete subject</a>
     </div>
 </div>
 
--- public/manage_content.php
+++ public/manage_content.php
@@ -6,6 +6,8 @@
 
 <div id="main">
     <div id="navigation">
+        <br>
+        <a href="admin.php">&laquo; Main Menu</a><br>
         <?php echo navigation($current_subject, $current_page); ?>
         <br />
         <a href="new_subject.php">+ Add a subject</a>
@@ -14,13 +16,21 @@
         <?php echo message(); ?>
         <?php if ($current_subject) { ?>
             <h2>Manage Subject</h2>
-            Menu name: <?php echo $current_subject["menu_name"]; ?><br />
-            <a href="edit_subject.php?subject=<?php echo $current_subject["id"]; ?>">Edit subject</a>
+            Menu name: <?php echo htmlentities($current_subject["menu_name"]); ?><br />
+            Position: <?php echo htmlentities($current_subject["position"]); ?><br />
+            Visible: <?php echo htmlentities($current_subject["visible"] == 1 ? 'yes' : 'no'); ?><br />
+            <br />
+            <a href="edit_subject.php?subject=<?php echo urlencode($current_subject["id"]); ?>">Edit subject</a>
 
         <?php } elseif ($current_page) { ?>
             <h2>Manage Page</h2>
-            Menu name: <?php echo $current_page["menu_name"]; ?><br />
-
+            Menu name: <?php echo htmlentities($current_page["menu_name"]); ?><br />
+            Position: <?php echo htmlentities($current_page["position"]); ?><br />
+            Visible: <?php echo htmlentities($current_page["visible"] == 1 ? 'yes' : 'no'); ?><br />
+            Content: <br />
+            <div class="view-content">
+                <?php echo htmlentities($current_page["content"]); ?>
+            </div>
         <?php } else { ?>
             Please select a subject or a page.
         <?php } ?>
