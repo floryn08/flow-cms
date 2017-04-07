fa24899ef0ac7e2eb309edbb5e973b743f5bfd10
Flow <floryn.euro.2007@gmail.com>
2017-04-07 13:43:26 +0300

    github pages initial commit

--- public/logout.php
+++
@@ -1,9 +0,0 @@
-<?php require_once("../includes/session.php"); ?>
-<?php require_once("../includes/functions.php"); ?>
-
-<?php //v1
-$_SESSION["admin_id"] = null;
-$_SESSION["username"] = null;
-redirect_to("login.php");
-
-?>
